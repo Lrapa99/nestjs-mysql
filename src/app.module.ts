550e670bbb9bb0187be388bd09131dbc27092cbb
lrapa99 <luisrapalino88@gmail.com>
2023-03-01 14:45:29 -0500

    serve static added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: DB_HOST,
